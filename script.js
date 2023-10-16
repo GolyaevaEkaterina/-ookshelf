728f0686f089d3dcb4a3d842bb33327944f59346
GolyaevaEkaterina <katrin.alien1@gmail.com>
2023-10-16 22:59:00 +0300

    Добавление новой книги

--- script.js
+++ script.js
@@ -27,35 +27,62 @@ const books = [
 
     ]
 
-    const container = document.getElementById("container-books")
+const container = document.getElementById("container-books")
     
-    books.forEach((book) => {
-        container.innerHTML += `
-        <div class="book">
-           <h3 class="book__title">${book.title}</h3>
-           <p class="book__author">${book.authors}</p>
-           <p class="book__year">${book.year}</p>
-           <img class="book__image" src="${book.image}"/>
-           <div class="book__buttons">
-             <button class="book__button">Изменить</button>
-             <button class="book__button">Удалить</button>
-           </div>
-        </div>
-           `
-    })
-
-
-    let isOpen = false
+function renderBooks(){
+  container.innerHTML = ""
+  books.forEach((book) => {
+    container.innerHTML += `
+    <div class="book">
+       <h3 class="book__title">${book.title}</h3>
+       <p class="book__author">${book.authors}</p>
+       <p class="book__year">${book.year}</p>
+       <img class="book__image" src="${book.image}"/>
+       <div class="book__buttons">
+         <button class="book__button">Изменить</button>
+         <button class="book__button">Удалить</button>
+       </div>
+    </div>
+       `
+  })
+}
 
 function openForm(){
-    const bookForm = document.getElementById("section-dop")
+    const bookForm = document.getElementById("Form")
     
-    if (isOpen){
-       bookForm.style.display = "flex"
-       isOpen = true
-    }else{
-        bookForm.style.display = "none"
-        isOpen = false
-    }
+    bookForm.style.display = "flex"   
+}
 
+function closeForm(){
+  const bookForm = document.getElementById("Form")
+  
+  bookForm.style.display = "none"   
+}
+
+function addBook(){
+  const Title = document.getElementById('bookTitle').value
+  const Autor = document.getElementById('bookAutor').value
+  const Year = document.getElementById('bookYear').value
+  const Image = document.getElementById('bookImage').value
+  
+  const book = {
+      title: Title,
+      authors: Autor,
+      year: Year,
+      image: Image,
   }
+
+  books.push(book)
+
+  renderBooks()
+  
+  closeForm()
+
+  document.getElementById('bookTitle').value = ""
+  document.getElementById('bookAutor').value = ""
+  document.getElementById('bookYear').value = ""
+  document.getElementById('bookImage').value = ""
+
+}
+
+renderBooks()
