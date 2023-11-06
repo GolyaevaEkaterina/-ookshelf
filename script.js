649b22da64392d3ec773c16fbb5060aebdcb8c48
GolyaevaEkaterina <katrin.alien1@gmail.com>
2023-11-06 22:50:02 +0300

    Кнопки

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+let counter = 1
+
 let books = [
     {
-      id: 1,
+      id: counter++,
       title: 'Отверженные',
       authors: 'Виктор Мари Гюго',
       year: '1862',
       image: '/images/Hugo.jpg'
     },
     {
-      id: 2,
+      id: counter++,
       title: 'Братья Карамазовы',
       authors: 'Федор Михайлович Достоевский',
       year: '1880',
       image: '/images/Dostoevsky.png'
     },
     {
-      id: 3,
+      id: counter++,
       title:'Мартин Иден',
       authors: 'Джек Лондон',
       year: '1909',
       image:'/images/London.jpeg'
     },
     {
-      id: 4,
+      id: counter++,
       title:'Герой нашего времени',
       authors: 'Михаил Юрьевич Лермонтов',
       year: 1840,
@@ -33,91 +35,95 @@ let books = [
 const container = document.getElementById("container-books")
     
 function renderBooks(){
-  container.innerHTML = ""
-  books.forEach((book) => {
-    container.innerHTML += `
-    <div class="book">
-       <h3 class="book__title">${book.title}</h3>
-       <p class="book__author">${book.authors}</p>
-       <p class="book__year">${book.year}</p>
-       <img class="book__image" src="${book.image}"/>
-       <div class="book__buttons">
-         <button class="book__button" onclick="openModalEditBook()"  id="OpenForm-EditBook">Изменить</button>
-         <button onclick="deleteBook(${book.id})" class="book__button book__button_delete">Удалить</button>
-       </div>
-    </div>
-       `
-  })
+    container.innerHTML = ""
+    books.forEach((book) => {
+      console.log(book.id)
+      container.innerHTML += `
+      <div class="book">
+         <h3 class="book__title">${book.title}</h3>
+         <p class="book__author">${book.authors}</p>
+         <p class="book__year">${book.year}</p>
+         <img class="book__image" src="${book.image}"/>
+         <div class="book__buttons">
+           <button class="book__button" id="editBook__Open-form-${book.id}">Изменить</button>
+           <button class="book__button" id="deleteBook__button-${book.id}">Удалить</button>
+         </div>
+      </div>
+         `
+    })
+
+   // books.forEach((book) => {
+   //   const buttonDeleteBook = document.getElementById('deleteBook__button-${book.id}')
+   //   const makeDeleteBook = () => deleteBook(book.id)
+   //   buttonDeleteBook.addEventListener('click', makeDeleteBook)
+   // })
 }
 
 function addToLocalStorage(){
-  const booksJson = JSON.stringify (books)
-  localStorage.setItem("books", booksJson)  //Какая разница между "" и '' ?//
+    const booksJson = JSON.stringify (books)
+    localStorage.setItem("books", booksJson)  //Какая разница между "" и '' ?//
 }
 
 function openModalNewBook(){
     const bookForm = document.getElementById("newBook")
-    
     bookForm.style.display = "flex"   
 }
 
 function closeModalNewBook(){
-  const bookForm = document.getElementById("newBook")
-  
-  bookForm.style.display = "none"   
+    const bookForm = document.getElementById("newBook")
+    bookForm.style.display = "none"   
 }
 
 function openModalEditBook(){
-  const bookEditForm = document.getElementById("editBook")
-  
-  bookEditForm.style.display = "flex"   
+    const bookEditForm = document.getElementById("editBook")
+    bookEditForm.style.display = "flex"   
 }
 
 function closeModalEditBook(){
-const bookEditForm = document.getElementById("editBook")
-
-bookEditForm.style.display = "none"   
+    const bookEditForm = document.getElementById("editBook")
+    bookEditForm.style.display = "none"   
 }
 
 function clearForm(){
-  document.getElementById('bookTitle').value = ""
-  document.getElementById('bookAutor').value = ""
-  document.getElementById('bookYear').value = ""
-  document.getElementById('bookImage').value = ""
+    document.getElementById('bookTitle').value = ""
+    document.getElementById('bookAutor').value = ""
+    document.getElementById('bookYear').value = ""
+    document.getElementById('bookImage').value = ""
 }
 
 function addBook(){
-  const Title = document.getElementById('bookTitle').value
-  const Autor = document.getElementById('bookAutor').value
-  const Year = document.getElementById('bookYear').value
-  const Image = document.getElementById('bookImage').value
+    const title = document.getElementById('bookTitle').value
+    const autor = document.getElementById('bookAutor').value
+    const year = document.getElementById('bookYear').value
+    const image = document.getElementById('bookImage').value
   
-  const book = {
-      title: Title,
-      authors: Autor,
-      year: Year,
-      image: Image,
-  }
+    const book = {
+      id: counter++,
+      title: title,
+      authors: autor,
+      year: year,
+      image: image,
+    }
 
-  books.push(book)
+    books.push(book)
 
-  renderBooks()
-  closeModalNewBook()
-  clearForm()
-  addToLocalStorage()
+    renderBooks()
+    closeModalNewBook()
+    clearForm()
+    addToLocalStorage()
 }
 
 function deleteBook(id){
-  const book = books.find((b) => {
-    return b.id === id
-  })
+    const book = books.find((b) => {
+      return b.id === id
+    })
 
-  const bookIndex = books.indexOf(book)
+    const bookIndex = books.indexOf(book)
 
-  books.splice(bookIndex, 1)
+    books.splice(bookIndex, 1)
 
-  renderBooks()
-  addToLocalStorage() 
+    renderBooks()
+    addToLocalStorage() 
 }
 
 const booksJson = localStorage.getItem("books")
@@ -125,19 +131,25 @@ const savedBooks = JSON.parse (booksJson)
 if (booksJson) {
   books = savedBooks
 }
+
 renderBooks()
 
-const buttonOpenForm = document.getElementById('OpenForm-button')
-buttonOpenForm.addEventListener('click', openModalNewBook)
+const buttonOpenFormNewBook = document.getElementById('newBook__Open-button')
+buttonOpenFormNewBook.addEventListener('click', openModalNewBook)
 
-const buttonCloseForm = document.getElementById('newBook__Close-button')
-buttonCloseForm.addEventListener('click', closeModalNewBook)
+const buttonCloseFormNewBook = document.getElementById('newBook__Close-button')
+buttonCloseFormNewBook.addEventListener('click', closeModalNewBook)
 
 const buttonAddBook = document.getElementById('newBook__Add-book')
 buttonAddBook.addEventListener('click', addBook)
 
-//const buttonOpenFormEditBook = document.getElementById('OpenForm-EditBook')
-//buttonOpenFormEditBook.addEventListener('click', openModalEditBook)//  -- не работает код
 
-//const buttonDeleteBook = document.getElementsByClassName('book__button_delete')
-//buttonDeleteBook.addEventListener('click', deleteBook)// -- не работает код
+
+
+const buttonCloseFormEditBook = document.getElementById('editBook__Close-button')
+buttonCloseFormEditBook.addEventListener('click', closeModalEditBook)
+
+
+//const buttonOpenFormEditBook = document.getElementById('editBook__Open-form-${book.id}')
+      //buttonOpenFormEditBook.addEventListener('click', openModalEditBook)//  -- не работает код
+
